9d12910057d193e24f97f231a343e4bebd2c9ac9
tkaburagi <t.kaburagi@me.com>
2021-01-25 16:57:26 +0900

    enable mesh gw

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gcloud container clusters get-credentials lab-cluster --zone asia-northeast1-a --project se-kabu
+gcloud container clusters get-credentials lab-cluster --zone us-central1-c --project se-kabu
 
 if [ $1 = 1 ]; then
   kubectl delete -f corp.yaml -n=multicluster-servicemesh
@@ -12,7 +12,7 @@ elif [ $1 = 0 ]; then
   kubectl get all -n=multicluster-servicemesh
 fi
 
-#aws eks --region ap-northeast-1 update-kubeconfig --name consul-mesh-gateway-cluster-2
+aws eks --region ap-northeast-1 update-kubeconfig --name consul-mesh-gateway-cluster-2
 
 if [ $1 = 1 ]; then
   kubectl delete -f japan.yaml -n=multicluster-servicemesh
